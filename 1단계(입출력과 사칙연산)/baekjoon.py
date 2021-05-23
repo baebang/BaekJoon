ee136170338dac9edfe32100deb2818e294f01d0
baebang <wowjd2524@gmail.com>
2021-05-23 19:02:14 +0900

    1 commit

---
+++ 1단계(입출력과 사칙연산)/baekjoon.py
@@ -0,0 +1,34 @@
+#number 1000
+a, b = map(int, input().split())
+print(a+b)
+
+#number 2557
+print("Hello World!")
+
+#number 2558
+A = int(input())
+B = int(input())
+print(A*(B%10), A*((B//10)%10), A*(B//100),A*B, sep='\n')
+
+#number 10430
+A,B,C = map(int,input().split())
+print((A+B)%C, ((A%C) + (B%C))%C, (A*B)%C,((A%C) * (B%C))%C, sep='\n')
+
+#number 10869
+A,B = map(int,input().split())
+print(A+B, A-B, A*B, A//B, A%B, sep='\n')
+
+#number 1008
+a, b = map(int, input().split())
+print(a/b)
+
+#number 10998
+a, b = map(int, input().split())
+print(a*b)
+
+#number 1001
+a, b = map(int, input().split())
+print(a-b)
+
+#number 10718
+print("강한친구 대한육군\n강한친구 대한육군")
