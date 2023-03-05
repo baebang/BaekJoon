3e6160484d213f77640c86e4d2734c0c9638fc36
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-03-05 21:35:59 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,10 @@
+count = int(input())
+arr = []
+listf = []
+for _ in range(count):
+    a,b = map(str,input().split())
+    list(b)
+    for t in b:
+        for g in range(int(a)):
+            print(t,end="")
+    print("")
