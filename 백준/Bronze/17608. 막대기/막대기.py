ec60f1e8bc4500916926d3e46c3826d3ce36d2e6
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-03-12 00:54:00 +0900

    [Bronze II] Title: 막대기, Time: 92 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -1,14 +1,14 @@
 import sys
-
 N = int(sys.stdin.readline().strip())
 arr = [int(sys.stdin.readline().strip()) for _ in range(N)]
-max_val = arr[-1]
-last_idx = N - 1
+
+max = arr[-1]
+first = N-1
 count = 1
 
 for i in range(N):
-    if arr[last_idx - i] > max_val:
-        max_val = arr[last_idx - i]
+    if arr[first-i] > max:
+        max = arr[first-i]
         count += 1
 
 print(count)
